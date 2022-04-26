419d58c024b431045e5a6e03abbb336001e9f6c5
nouamane <nouamane@producement.com>
2022-04-26 18:42:08 +0000

    Fix failing test cases

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/helper/PaymentProviderWrapper.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/helper/PaymentProviderWrapper.kt
@@ -1,5 +1,6 @@
 package io.pleo.antaeus.core.helper
 
+import io.pleo.antaeus.core.exceptions.InsufficientBalanceException
 import io.pleo.antaeus.core.external.PaymentProvider
 import io.pleo.antaeus.models.Invoice
 import io.pleo.antaeus.models.InvoiceStatus
@@ -8,6 +9,9 @@ class PaymentProviderWrapper(
     private val paymentProvider: PaymentProvider
 ) {
     fun charge(invoice: Invoice): InvoiceStatus {
-
+        if (paymentProvider.charge(invoice))
+            return InvoiceStatus.PAID
+        else
+            throw InsufficientBalanceException(invoiceId = invoice.id, customerId = invoice.customerId)
     }
 }
